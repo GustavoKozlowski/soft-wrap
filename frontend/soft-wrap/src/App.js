c7dcfbb703b76dd0ab03f1e5c8bbc97b025cc354
Gustavo Kozlowski <gustavokozlowski96@gmail.com>
2022-03-17 11:02:11 -0300

    tabela com dados mockados

--- frontend/soft-wrap/src/App.js
+++ frontend/soft-wrap/src/App.js
@@ -1,11 +1,39 @@
 import Tabela from "./components/Tabela";
 
-function App() {
+export default function App() {
+  const dados = [
+    {
+      id: 1,
+      nome: "Gustavo Kozlowski",
+      idade: 25,
+      estadoCivil: "solteiro",
+      cpf: "158.457.896-50",
+      cidade: "Rio de Janeiro",
+      estado: "Rio de Janeiro",
+    },
+    {
+      id: 2,
+      nome: "J.Cole",
+      idade: 35,
+      estadoCivil: "solteiro",
+      cpf: "158.457.896-50",
+      cidade: "Taubaté",
+      estado: "São Paulo",
+    },
+    {
+      id: 3,
+      nome: "Vovô Jones",
+      idade: 50,
+      estadoCivil: "solteiro",
+      cpf: "158.457.896-50",
+      cidade: "L.A",
+      estado: "Califónia",
+    },
+  ];
   return (
     <>
-    <Tabela />
+      <Tabela pessoas={dados} />
     </>
   );
 }
 
-export default App;
--- frontend/soft-wrap/src/components/Tabela/index.js
+++ frontend/soft-wrap/src/components/Tabela/index.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Tabela(){
-    return (
-        <>
-        <h1>Teste 123</h1>
-        </>
-    )
+export default function Tabela({ pessoas }) {
+  return (
+    <>
+      <h1>Tabela de Dados</h1>
+      <Table striped bordered hover variant="primary">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Estado Civil</th>
+            <th>CPF</th>
+            <th>Cidade</th>
+            <th>Estado</th>
+            <th>Editar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pessoas.map((pessoa) => (
+            <tr key={pessoa.id}>
+              <td>{pessoa.id}</td>
+              <td>{pessoa.nome}</td>
+              <td>{pessoa.idade}</td>
+              <td>{pessoa.estadoCivil}</td>
+              <td>{pessoa.cpf}</td>
+              <td>{pessoa.cidade}</td>
+              <td>{pessoa.estado}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
 }
